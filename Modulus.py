9cfa3f2be23a3ab15764fe00b673dd4452fbd7be
Prashant Singh <prashantsinghsingh2009@gmail.com>
2026-03-23 00:12:07 +0530

    add new code

---
+++ Modulus.py
@@ -0,0 +1,10 @@
+# Modulus Program
+
+print("Modulus Program")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 % num2
+
+print("The remainder when", num1, "is divided by", num2, "is =", result)
