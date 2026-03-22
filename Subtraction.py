dbfae746cfeaecc1db41679f9c8e208709ca947c
Prashant Singh <prashantsinghsingh2009@gmail.com>
2026-03-23 00:09:22 +0530

    new code added

---
+++ Subtraction.py
@@ -0,0 +1,10 @@
+# Subtraction Program
+
+print("Subtraction Program")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 - num2
+
+print("The difference of", num1, "and", num2, "is =", result)
